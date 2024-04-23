2c5e12640adf3890e3524bece9fddefcbd94d2c2
jameshsu1125 <jameshsu1125@gmail.com>
2024-04-23 19:09:51 +0800

    Update mockServiceWorker.js, button/index.tsx, home/index.less, home/index.tsx, and index.tsx

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,26 +1,12 @@
-import { IReactProps, TransitionType } from '@/settings/type';
-import useTween from 'lesca-use-tween';
-import { useEffect } from 'react';
+import { IReactProps } from '@/settings/type';
 import Regular from './regular';
-import { Debug } from '@/settings/type-unity';
 
-type TRegularProps = Debug<{
+type TRegularProps = IReactProps & {
   onClick?: () => void;
-  transition?: TransitionType;
-}>;
+};
 
-const Button = ({ children, onClick, transition }: IReactProps & TRegularProps) => {
-  const [style, setStyle] = useTween({ opacity: 0 });
-
-  useEffect(() => {
-    if (transition === TransitionType.FadeIn) setStyle({ opacity: 1 });
-  }, [setStyle, transition]);
-
-  return (
-    <button style={style} onClick={onClick}>
-      {children}
-    </button>
-  );
+const Button = ({ children, onClick }: TRegularProps) => {
+  return <button onClick={onClick}>{children}</button>;
 };
 
 Button.regular = Regular;
