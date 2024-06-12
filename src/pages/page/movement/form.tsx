22f3da0b804ac500f6eb983f9f7e1304a82b5548
jameshsu1125 <jameshsu1125@gmail.com>
2024-06-12 20:06:20 +0800

    chore: Optimize code structure and update image dimensions in movement form

--- src/pages/page/movement/form.tsx
+++ src/pages/page/movement/form.tsx
@@ -1,16 +1,58 @@
 import Article from '@/components/article';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import './form.less';
+import { Data, MAINTAIN_COST_BY_MILE } from './config';
+import Counter from './counter';
 
-const Form = memo(() => (
-  <div className='Form'>
-    <Article expend>
-      <div className='flex w-full justify-center'>
-        <div className='w-full max-w-[1100px]'>
-          <div className='image' />
+const Form = memo(({ data, mile }: { data: Data; mile: number }) => {
+  const maintainCost = useMemo(() => {
+    return MAINTAIN_COST_BY_MILE(mile);
+  }, [mile]);
+
+  const taxCost = useMemo(() => {
+    if (mile === 0) return 0;
+    return 11920;
+  }, [mile]);
+
+  const totalCost = useMemo(() => {
+    if (mile === 0) return 0;
+    return (data.savedCostYearly || 0) + 11920 + maintainCost;
+  }, [mile, maintainCost, data.savedCostYearly]);
+
+  return (
+    <div className='Form'>
+      <Article expend>
+        <div className='flex w-full justify-center'>
+          <div className='w-full max-w-[1024px]'>
+            <div className='image'>
+              <div>
+                <div className='total'>
+                  <span>
+                    <Counter init={0} to={totalCost} max={99999999} dollar />
+                  </span>
+                  元
+                </div>
+                <div className='fuel'>
+                  <span>
+                    <Counter init={0} to={data.savedCostYearly || 0} max={99999999} dollar />
+                  </span>
+                </div>
+                <div className='tax'>
+                  <span>
+                    <Counter init={0} to={taxCost} max={99999999} dollar />
+                  </span>
+                </div>
+                <div className='maintain'>
+                  <span>
+                    <Counter init={0} to={maintainCost || 0} max={99999999} dollar />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </Article>
-  </div>
-));
+      </Article>
+    </div>
+  );
+});
 export default Form;
--- src/pages/page/movement/result.tsx
+++ src/pages/page/movement/result.tsx
@@ -1,38 +1,13 @@
 import Article from '@/components/article';
-import { memo, useContext, useMemo } from 'react';
+import useMedia, { MediaType } from '@/hooks/useMedia';
+import { memo } from 'react';
+import { Data } from './config';
 import './result.less';
 import ResultDesktop from './resultDesktop';
-import useMedia, { MediaType } from '@/hooks/useMedia';
 import ResultMobile from './resultMobile';
-import { MovementContext } from './config';
 
-export type Data = {
-  chargeDaily?: number;
-  chargeMeekly?: number;
-  electricityCost?: number;
-  fuelCost?: number;
-  savedCostYearly?: number;
-};
-
-const Result = memo(() => {
+const Result = memo(({ data }: { data: Data }) => {
   const [device] = useMedia();
-  const [state] = useContext(MovementContext);
-
-  const data = useMemo(() => {
-    const chargeDaily = Math.round(state.mile / 567);
-    const chargeMeekly = Math.round((state.mile * 7) / 567);
-    const electricityCost = Math.round(state.mile * 0.8);
-    const fuelCost = Math.round(state.mile * 2.5);
-    const savedCostYearly = (fuelCost - electricityCost) * 365 + (state.mile === 0 ? 0 : 17410);
-    return {
-      chargeDaily,
-      chargeMeekly,
-      electricityCost,
-      fuelCost,
-      savedCostYearly,
-    };
-  }, [state.mile]);
-
   return (
     <div className='Result'>
       <Article expend>
