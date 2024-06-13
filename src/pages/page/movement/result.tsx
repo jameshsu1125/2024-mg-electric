463feb5198874dfc5c4bc6c8d7df5e7ace9ad342
jameshsu1125 <jameshsu1125@gmail.com>
2024-06-13 22:16:23 +0800

    Optimize code structure and calculate yearly saved cost based on mileage

--- src/pages/page/movement/result.tsx
+++ src/pages/page/movement/result.tsx
@@ -23,7 +23,9 @@ const Result = memo(() => {
     const chargeMeekly = Math.round((state.mile * 7) / 567);
     const electricityCost = Math.round(state.mile * 0.8);
     const fuelCost = Math.round(state.mile * 2.5);
-    const savedCostYearly = (fuelCost - electricityCost) * 365 + (state.mile === 0 ? 0 : 17410);
+    const savedCostYearly = Math.round(
+      (state.mile * 2.5 - state.mile * 0.8) * 365,
+    ); /*+  (state.mile === 0 ? 0 : 17410) */
     return {
       chargeDaily,
       chargeMeekly,
