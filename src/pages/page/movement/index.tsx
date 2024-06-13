9259c7a91c23ae5ef67e114ed97e0f2cda125949
jameshsu1125 <jameshsu1125@gmail.com>
2024-06-13 22:12:23 +0800

    chore: Optimize code structure and update image dimensions in movement form

--- src/pages/page/movement/index.tsx
+++ src/pages/page/movement/index.tsx
@@ -11,12 +11,12 @@ const Movement = memo(() => {
   const [state] = value;
 
   const data = useMemo(() => {
-    const chargeDaily = Math.round(state.mile / 567);
-    const chargeMeekly = Math.round((state.mile * 7) / 567);
-    const electricityCost = Math.round(state.mile * 0.8);
-    const fuelCost = Math.round(state.mile * 2.5);
-    const savedCostYearly =
-      (fuelCost - electricityCost) * 365; /*+  (state.mile === 0 ? 0 : 17410) */
+    const chargeDaily = Math.round(state.mile / 567); // 每天充電次數
+    const chargeMeekly = Math.round((state.mile * 7) / 567); // 每週充電次數
+    const electricityCost = Math.round(state.mile * 0.8); // 每天充電成本
+    const fuelCost = Math.round(state.mile * 2.5); // 每天油耗成本
+    const savedCostYearly = // 每年省下來的成本
+      (state.mile * 2.5 - state.mile * 2.5) * 365; /*+  (state.mile === 0 ? 0 : 17410) */
     return {
       chargeDaily,
       chargeMeekly,
