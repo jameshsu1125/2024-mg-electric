e1bbf8cd4fb8445e583449fc21837bc86ce1e248
jameshsu1125 <jameshsu1125@gmail.com>
2024-06-14 10:46:36 +0800

    Update config.tsx

--- src/pages/page/movement/config.tsx
+++ src/pages/page/movement/config.tsx
@@ -15,15 +15,15 @@ export type Data = {
 };
 
 export const MAINTAIN_COST_BY_MILE = (mile: number) => {
-  if (mile === 0) return 0;
-  else if (mile <= 10000) return 3967;
-  else if (mile <= 20000) return 7786;
-  else if (mile <= 30000) return 13185;
-  else if (mile <= 40000) return 21154;
-  else if (mile <= 50000) return 25121;
-  else if (mile <= 60000) return 28070;
-  else if (mile <= 70000) return 32469;
-  else if (mile <= 80000) return 37358;
-  else if (mile <= 90000) return 41325;
-  else return 44314;
+  if (mile < 10000) return 0;
+  else if (mile < 20000) return 3967;
+  else if (mile < 30000) return 7786;
+  else if (mile < 40000) return 13185;
+  else if (mile < 50000) return 21154;
+  else if (mile < 60000) return 25121;
+  else if (mile < 70000) return 28070;
+  else if (mile < 80000) return 32469;
+  else if (mile < 90000) return 37358;
+  else if (mile < 100000) return 44314;
+  return 44314;
 };
